48e9e48be897a237c2f89e11bd522c29844df4f7
devD <cloudsperk3000@gmail.com>
2026-04-13 00:40:48 +0530

    fixed name

--- 01-Terraform-Command-Basics/c8-02-bastion-host-linuxvm.tf
+++ 01-Terraform-Command-Basics/c8-02-bastion-host-linuxvm.tf
@@ -10,8 +10,8 @@ resource "azurerm_public_ip" "bastion_host_public_ip" {
 # Resource 2 - Create network interface
 resource "azurerm_network_interface" "bastion_host_linuxvm_nic" {
   name = "${local.resource_name_prefix}-bastion-host-linuxvm-nic"
-  location = azurerm_resource_group.rg.name
-  resource_group_name = azurerm_resource_group.rg.location
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name = "bastion-host-ip-1"
